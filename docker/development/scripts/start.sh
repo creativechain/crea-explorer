727c4a7aad6b6f9064fd393684a1f4545936540c
Andrew Chaney (netuoso) <andrewc@pobox.com>
2018-06-29 15:21:30 -0500

    Add missing PHP dependency. Modify composer hash.

--- docker/development/scripts/start.sh
+++ docker/development/scripts/start.sh
@@ -9,8 +9,8 @@ echo '-- Setting nginx group to use gid '$TARGET_GID
 groupmod -o -g $TARGET_GID nginx || true
 
 echo '-- Updating composer libraries'
-composer -d=/var/www/html install
-composer -d=/var/www/html update
+composer -d/var/www/html install
+composer -d/var/www/html update
 
 echo '* Starting nginx'
 /usr/bin/supervisord -n -c /etc/supervisord.conf
