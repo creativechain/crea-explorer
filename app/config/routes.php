9e47ac0099c42bf290593790c5ef4242f594637e
Andrew Chaney (netuoso) <andrewc@pobox.com>
2018-07-01 12:51:05 -0500

    Add stats routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -54,6 +54,15 @@ $router->add('/block/([a-zA-Z0-9]+)', [
 ])->setName("block-view");
 
 /*
+  charts routes
+*/
+
+$router->add('/stats', [
+  'controller' => 'charts',
+  'action' => 'index'
+]);
+
+/*
   comment viewing routes
 */
 
