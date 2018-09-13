9b9732fb9a18c507713c3a632713fb7e058fa964
Andrew Chaney (netuoso) <andrewc@pobox.com>
2018-09-12 19:07:08 -0500

    no method get_followers/following

--- docker/sync/sync.py
+++ docker/sync/sync.py
@@ -309,33 +309,33 @@ def update_account(account_name):
       return
     # Get Account Data
     account = collections.OrderedDict(sorted(state[0].items()))
-    # Get followers
-    account['followers'] = []
-    account['followers_count'] = 0
-    account['followers_mvest'] = 0
-    followers_results = stm.rpc.get_followers(account_name, "", "blog", 100)
-    while followers_results:
-      last_account = ""
-      for follower in followers_results:
-        last_account = follower['follower']
-        if 'blog' in follower['what'] or 'posts' in follower['what']:
-          account['followers'].append(follower['follower'])
-          account['followers_count'] += 1
-          if follower['follower'] in mvest_per_account.keys():
-            account['followers_mvest'] += float(mvest_per_account[follower['follower']])
-      followers_results = stm.rpc.get_followers(account_name, last_account, "blog", 100)[1:]
-    # Get following
-    account['following'] = []
-    account['following_count'] = 0
-    following_results = stm.rpc.get_following(account_name, -1, "blog", 100)
-    while following_results:
-      last_account = ""
-      for following in following_results:
-        last_account = following['following']
-        if 'blog' in following['what'] or 'posts' in following['what']:
-          account['following'].append(following['following'])
-          account['following_count'] += 1
-      following_results = stm.rpc.get_following(account_name, last_account, "blog", 100)[1:]
+    # # Get followers
+    # account['followers'] = []
+    # account['followers_count'] = 0
+    # account['followers_mvest'] = 0
+    # followers_results = stm.rpc.get_followers(account_name, "", "blog", 100)
+    # while followers_results:
+    #   last_account = ""
+    #   for follower in followers_results:
+    #     last_account = follower['follower']
+    #     if 'blog' in follower['what'] or 'posts' in follower['what']:
+    #       account['followers'].append(follower['follower'])
+    #       account['followers_count'] += 1
+    #       if follower['follower'] in mvest_per_account.keys():
+    #         account['followers_mvest'] += float(mvest_per_account[follower['follower']])
+    #   followers_results = stm.rpc.get_followers(account_name, last_account, "blog", 100)[1:]
+    # # Get following
+    # account['following'] = []
+    # account['following_count'] = 0
+    # following_results = stm.rpc.get_following(account_name, -1, "blog", 100)
+    # while following_results:
+    #   last_account = ""
+    #   for following in following_results:
+    #     last_account = following['following']
+    #     if 'blog' in following['what'] or 'posts' in following['what']:
+    #       account['following'].append(following['following'])
+    #       account['following_count'] += 1
+    #   following_results = stm.rpc.get_following(account_name, last_account, "blog", 100)[1:]
     # Convert to Numbers
     account['proxy_witness'] = float(account['proxied_vsf_votes'][0]) / 1000000
     for key in ['lifetime_bandwidth', 'reputation', 'to_withdraw']:
