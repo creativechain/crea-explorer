7b0cd7ae197e54d4a6ddbf94f5bad53cb119fd54
ander7agar <ander7agar@gmail.com>
2019-02-01 20:51:46 +0100

    fixed cread on controllers

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -7,4 +7,18 @@ use Phalcon\Mvc\Dispatcher;
 class ControllerBase extends Controller
 {
 
+    /**
+     * @var \Cread
+     */
+    protected $cread;
+
+    /**
+     * ControllerBase constructor.
+     */
+    public function __construct()
+    {
+        $this->cread = new \Cread('https://node1.creary.net');
+    }
+
+
 }
