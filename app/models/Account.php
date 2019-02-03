f89a10c59ab16b6e8ef2885066ec00998fc9567c
ander7agar <ander7agar@gmail.com>
2019-02-03 23:03:02 +0100

    fixed profile image

--- app/models/Account.php
+++ app/models/Account.php
@@ -8,8 +8,8 @@ class Account extends Document
     if(property_exists($this, 'json_metadata') && !is_array($this->json_metadata)) {
         $this->json_metadata = json_decode($this->json_metadata, true);
     }
-    if(property_exists($this, 'json_metadata') && isset($this->json_metadata['profile']) && isset($this->json_metadata['profile']['profile_image'])) {
-        return $this->json_metadata['profile']['profile_image'];
+    if(property_exists($this, 'json_metadata') && isset($this->json_metadata['avatar']) && isset($this->json_metadata['avatar']['hash'])) {
+        return 'https://ipfs.creary.net/ipfs/' . $this->json_metadata['avatar']['hash'];
     }
     return null;
   }
